77b0144c3c3fa29dcb4845eeb9b9e7186d60a820
2001010156-Nicholaslow <20010156@myrp.edu.sg>
2022-06-04 18:19:33 +0800

    Partner_update

--- client/src/Partner/PartnerHome.js
+++ client/src/Partner/PartnerHome.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './PartnerHome.css';
-import { BiListUl, BiBarChartAlt2, BiSearchAlt} from "react-icons/bi";
+import { BiListUl, BiBarChartAlt2, BiSearchAlt, BiHide, BiUser } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 
@@ -17,14 +17,15 @@ function PartnerHome() {
                     <div className='stuff_todo'>
                         <div className='row_item'>
                             <div class="col-sm-12 col-md-6 col-lg-5">
-                                <h3>Choose an option</h3>
+                                <h3>Select an option</h3>
                             </div>
-                            <div>
+                            <div className='pages_options'>
                                 <div className='pages_icons'>
 
                                     <div className='icon'>
 
-                                        <Link to="./PartnerJobAd">
+
+                                        <Link to="../Partner/PartnerJobAd" style={{ padding: 10 }}>
                                             <button type="button">
                                                 <div className="image_icon">
                                                     <BiListUl />
@@ -34,7 +35,7 @@ function PartnerHome() {
                                         </Link>
 
 
-                                        <Link to="./PartnerUserSearch">
+                                        <Link to="../Partner/PartnerUserSearch" style={{ padding: 10 }}>
                                             <button type="button">
                                                 <div className="image_icon">
                                                     <BiSearchAlt />
@@ -45,7 +46,7 @@ function PartnerHome() {
 
 
 
-                                        <Link to="./PartnerStats">
+                                        <Link to="../Partner/PartnerStats" style={{ padding: 10 }}>
                                             <button type="button">
                                                 <div className="image_icon">
                                                     <BiBarChartAlt2 />
@@ -57,7 +58,29 @@ function PartnerHome() {
                                 </div>
 
                             </div>
+                            <div className='Profile_Edit'>
+                                <div className='icon'>
+                                    <Link to="../Partner/PartnerProfile" style={{ padding: 10, }}>
+                                        <button type="button">
+                                            <div className="image_icon">
+                                                <BiUser />
+                                            </div>
+                                            <p>Profile</p>
+                                        </button>
+                                    </Link>
 
+                                    <Link to="../Partner/Partner" style={{ padding: 10, }}>
+                                        <button type="button">
+                                            <div className="image_icon">
+                                                <BiHide />
+                                            </div>
+                                            <p>Add-on?</p>
+                                        </button>
+                                    </Link>
+
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
