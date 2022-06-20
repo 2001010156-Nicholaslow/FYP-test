4437bf0d326f5466e06ef7680cc35d138de9b0d3
2001010156-Nicholaslow <20010156@myrp.edu.sg>
2022-06-20 19:35:53 +0800

    20/6/22

--- client/src/Partner/Partner.js
+++ client/src/Partner/Partner.js
@@ -1,23 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BiHome } from "react-icons/bi";
 import Axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import PartnerHome from "./PartnerHome";
 import './Partner.css';
+import jwt_decode from 'jwt-decode';
+
 function Partner() {
 
     const id = localStorage.getItem("user_id");
+    const token = localStorage.getItem("token");
     const [msg, Setmsg] = useState("");
+    const [AllowUser, SetAllowUser] = useState(false);
 
-    Axios.post("http://localhost:3001/LoginCheckPartner", {
-        user_id: id
-    }).then((response) => {
-            Setmsg(response.data);
-    });
+    const nav = useNavigate();
+
+    const Exit = () => {
+        localStorage.clear()
+        sessionStorage.clear()
+        nav("../Login/PartnerLogin")
+    }
+
+    useEffect(() => {
+
+        var decoded = jwt_decode(token);
+
+        if (id == "" || token == "" || id == undefined || token == undefined) {
+            nav("../Login/PartnerLogin")
+        } else {
+            if (decoded.id == id) {
+                SetAllowUser(true)
+                Axios.post("http://localhost:3001/LoginCheckPartner", {
+                    user_id: id
+                }).then((response) => {
+                    Setmsg(response.data);
+                });
+            } else {
+                SetAllowUser(false)
+                localStorage.clear()
+                sessionStorage.clear()
+                nav("../Login/PartnerLogin")
+            }
+        }
+    }, [])
 
     return (
         <div>
+            {AllowUser}
             <Navbar bg="dark" variant="dark">
                 <Nav.Link href="./Partner">
                     <div className="image_icon_nav">
@@ -33,7 +64,7 @@ function Partner() {
                     <NavDropdown title={"Sign in as : " + msg} id="basic-nav-dropdown">
                         <NavDropdown.Item href="./PartnerProfile">Edit profile</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Log out</NavDropdown.Item>
+                        <NavDropdown.Item onClick={Exit}>Log Out</NavDropdown.Item>
                     </NavDropdown>
                 </Navbar.Collapse>
             </Navbar>
