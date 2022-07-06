d5f97deefe3c15606637d072c87d007525bbd4f1
IsaacRP <87507173+Isaac300700@users.noreply.github.com>
2022-07-06 12:00:53 +0800

    6/7
    Comment out the codes

--- frontend_admin/src/Pages/Stats/Stats.js
+++ frontend_admin/src/Pages/Stats/Stats.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from "react";
-import LineChart from "../../Components/LineChart";
-import { UserData } from "../../Data/Data";
+// import React, { useState, useEffect } from "react";
+// import LineChart from "../../Components/LineChart";
+// import { UserData } from "../../Data/Data";
 
-function App() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
+// function App() {
+//   const [userData, setUserData] = useState({
+//     labels: UserData.map((data) => data.year),
+//     datasets: [
+//       {
+//         label: "Users Gained",
+//         data: UserData.map((data) => data.userGain),
+//         backgroundColor: [
+//           "rgba(75,192,192,1)",
+//           "#ecf0f1",
+//           "#50AF95",
+//           "#f3ba2f",
+//           "#2a71d0",
+//         ],
+//         borderColor: "black",
+//         borderWidth: 2,
+//       },
+//     ],
+//   });
 
-  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
+//   // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
 
-  return (
-    <div className="App">
-      <div style={{ width: 700 }}>
-        <LineChart chartData={userData} />
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="App">
+//       <div style={{ width: 700 }}>
+//         <LineChart chartData={userData} />
+//       </div>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
