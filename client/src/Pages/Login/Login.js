0a6164de8c43f1bfc28bbb89e110e761ebec1160
Isaac20044098 <87507173+Isaac300700@users.noreply.github.com>
2022-07-11 20:14:36 +0800

    Merge works

---
+++ client/src/Pages/Login/Login.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Login.css";
+import Axios from "axios";
+import { decodeToken } from "react-jwt";
+export default function Login() {
+  const [email, setEmail] = useState("Isaac@gmail.com");
+  const [password, setPassword] = useState("password");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    Axios.post("http://localhost:3001/login", {
+      email: email,
+      password: password,
+    })
+      .then((response) => {
+        console.log(response.status);
+        if (response.status == 200) {
+          alert("Sign in sucessfully ");
+        }
+        localStorage.setItem("token", response.data.token);
+        window.location.assign("/admin/mainpage");
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.response.status == 401) {
+          alert("Wrong credentials");
+        } else {
+          alert("Something went wrong");
+        }
+      });
+    event.preventDefault();
+  }
+
+  return (
+    <Form onSubmit={handleSubmit} className="Form">
+      <h1>Admin Panel</h1>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter email"
+          required
+        />
+        <Form.Text className="text-muted"></Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+          required
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,50 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
+import { BrowserRouter as Router, Routes, Route,Outlet,Navigate} from "react-router-dom"; //dont remove router
+import Home from "./Home";
+import Login from "./Login/Login";
+import PartnerLogin from "./Login/PartnerLogin";
+import PartnerRegister from "./Register/PartnerRegister";
+import Profile from "./Profile";
+import YouthRegister from "./Register/YouthRegister"; //import from the js file
+import Partner from "./Partner/Partner";
+import PartnerProfile from "./Partner/PartnerProfile";
+import PartnerForm from "./Partner/PartnerForm";
+import PartnerJobAd from "./Partner/PartnerJobAd";
+import PartnerStats from "./Partner/PartnerStats";
+import PartnerUserSearch from "./Partner/PartnerUserSearch";
+import JobListing from "./JobListing";
+import PartnerFormEdit from "./Partner/PartnerFormEdit";
+import Welcome2 from "./Login/Welcome2";
+import Succes from "./success";
+//admin
+import AdminLogin from "./Pages/Login/Login";
+import User from "./Pages/User/User";
+import Users from "./Pages/Users/Users";
+import MainPage from "./Pages/MainPage/MainPage";
+import Opportunities from "./Pages/Opportunities/Opportunities";
+import Partners from "./Pages/Partners/Partners";
+import Reviews from "./Pages/Reviews/reviews";
+import Stats from "./Pages/Stats/Stats";
 
-import { BrowserRouter as Router,Routes, Route} from 'react-router-dom'; //dont remove router
-import Home from './Home';
-import Login from './Login/Login';
-import PartnerLogin from './Login/PartnerLogin';
-import PartnerRegister from './Register/PartnerRegister';
-import Profile from './Profile';
-import YouthRegister from './Register/YouthRegister'; //import from the js file
-import Partner from './Partner/Partner';
-import PartnerProfile from './Partner/PartnerProfile';
-import PartnerForm from './Partner/PartnerForm';
-import PartnerJobAd from './Partner/PartnerJobAd';
-import PartnerStats from './Partner/PartnerStats';
-import PartnerUserSearch from './Partner/PartnerUserSearch';
-import JobListing from './JobListing';
-import PartnerFormEdit from './Partner/PartnerFormEdit';
-import Welcome2 from './Login/Welcome2';
-import Succes from './success';
+const Requiredlogin = () => {
+  return localStorage.getItem("token") ? (
+    <Outlet />
+  ) : (
+    <Navigate replace to="/admin/login" />
+  );
+};
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
     <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="Login/login" element={<Login />} />
-          <Route path="Login/Partnerlogin" element={<PartnerLogin />} />
-          <Route path="/Register/partnerRegister" element={<PartnerRegister />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/success" element={<Succes />} />
-          <Route path="/JobListing" element={<JobListing />} />
-          <Route path="/Register/youthRegister" element={<YouthRegister />} />
-          <Route path="/Partner/Partner/" element={<Partner />} />
-          <Route path="/Partner/PartnerProfile" element={<PartnerProfile />}/>
-          <Route path="/Partner/PartnerForm" element={<PartnerForm />}/>
-          <Route path="/Partner/PartnerFormEdit" element={<PartnerFormEdit />}/>
-          <Route path="/Partner/PartnerJobAd" element={<PartnerJobAd />}/>
-          <Route path="/Partner/PartnerUserSearch" element={<PartnerUserSearch />}/>
-          <Route path="/confirm/:ConfirmationCode" element={<Welcome2 />} />
-
-        </Routes>
-  </BrowserRouter>,document.getElementById("root")
+      <Route path="/" element={<Home />} />
+      <Route path="Login/login" element={<Login />} />
+      <Route path="Login/Partnerlogin" element={<PartnerLogin />} />
+      <Route path="/Register/partnerRegister" element={<PartnerRegister />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/success" element={<Succes />} />
+      <Route path="/JobListing" element={<JobListing />} />
+      <Route path="/Register/youthRegister" element={<YouthRegister />} />
+      <Route path="/Partner/Partner/" element={<Partner />} />
+      <Route path="/Partner/PartnerProfile" element={<PartnerProfile />} />
+      <Route path="/Partner/PartnerForm" element={<PartnerForm />} />
+      <Route path="/Partner/PartnerFormEdit" element={<PartnerFormEdit />} />
+      <Route path="/Partner/PartnerJobAd" element={<PartnerJobAd />} />
+      <Route
+        path="/Partner/PartnerUserSearch"
+        element={<PartnerUserSearch />}
+      />
+      <Route path="/confirm/:ConfirmationCode" element={<Welcome2 />} />
+      <Route path="/admin/login" element={<AdminLogin />} />
+      <Route element={<Requiredlogin />}>
+        <Route path="/admin/mainpage" element={<MainPage />} />
+        <Route path="/admin/users" element={<Users />} />
+        <Route path="/admin/user/:userId" element={<User />} />
+        <Route path="/admin/opportunities" element={<Opportunities />} />
+        <Route path="/admin/partners" element={<Partners />} />
+        <Route path="/admin/reviews" element={<Reviews />} />
+        <Route path="/admin/stats" element={<Stats />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
