c02ba158e071d8d60b9933d0a253ab2b54ec4ecd
Nehal Aithal <20026044@myrp.edu.sg>
2022-07-20 18:12:09 +0800

    done navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
+import Login from './Login/Login';
+import PartnerRegister from './Register/PartnerRegister';
+import Profile from './Profile';
+import YouthRegister from './Register/YouthRegister'; //import from the js file
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'; //dont remove router
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import NavbarComp from './components/NavbarComp';
 
 function App() {
-  
-  return (
-    <div>
-      <ul className="App">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="Login/login">Login</Link></li>
-        <li><Link to="/Register/youthRegister">Youth Register</Link></li>
-        <li><Link to="/Register/PartnerRegister">Partner Register</Link></li>
-        <li><Link to="/profile">My Profile</Link></li>
-      </ul>
-
+  return(
+  <div>
+    {/* <ul className="App">
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="Login/login">Login</Link></li>
+      <li><Link to="/Register/youthRegister">Register</Link></li>
+      <li><Link to="/profile">My Profile</Link></li>
+    </ul>
+    <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="Login/login" element={<Login />} />
+          <Route path="/Register/partnerRegister" element={<PartnerRegister />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/Register/youthRegister" element={<YouthRegister />} />
+        </Routes> */}
+            <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="Login/login" element={<Login />} />
+          <Route path="/Register/partnerRegister" element={<PartnerRegister />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/Register/youthRegister" element={<YouthRegister />} />
+        </Routes>
+        { <NavbarComp/> }
     </div>);
 }
-
 
 export default App;
