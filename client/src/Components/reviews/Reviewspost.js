09abc1328624560f2521051eefc9f63ee036cf71
2001010156-Nicholaslow <20010156@myrp.edu.sg>
2022-07-21 22:35:10 +0800

    21/7/22

--- client/src/Components/reviews/Reviewspost.js
+++ client/src/Components/reviews/Reviewspost.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import StarRatings from 'react-star-ratings';
 
-const Reviewspost = ({Rposts, loading }) => {
+const Reviewspost = ({ Rposts, loading }) => {
   if (loading) {
     return <h2>Loading...</h2>;
   }
@@ -8,8 +9,17 @@ const Reviewspost = ({Rposts, loading }) => {
   return (
     <div className='list-group mb-4'>
       {Rposts.map((Rpost) => (
-        <div key={Rpost.id} className='list-group-item'>
-          {Rpost.title}
+        <div key={Rpost.review_id} className='list-group-item'>
+          <div className='reviews_box'>
+            <StarRatings
+              rating={Rpost.rating}
+              starDimension="30px"
+              starSpacing="10px"
+            />
+
+            {Rpost.review}
+          </div>
+
         </div>
       ))}
     </div>
